fe5d00f192c08392f95f9c0bad263a0b22c789a8
Miguel Gil Garcia <mgilgar@gmail.com>
2014-09-11 13:23:16 +0200

    2 methods refactored into 1 because they are very similar. Tests still pass.

--- src/fragment/submissions/MiguelGil.java
+++ src/fragment/submissions/MiguelGil.java
@@ -4,9 +4,40 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class MiguelGil {
+
+	private static final String EMPTY_STRING = "";
+	private static final String FRAGMENT_SEPARATOR = ";";
+
+	public class ConcatenationResult {
+		private String cancanatedString;
+		private int numberOfOverlappingChars;
+
+		public ConcatenationResult(String cancanatedString, int numberOfOverlappingChars) {
+			super();
+			this.cancanatedString = cancanatedString;
+			this.numberOfOverlappingChars = numberOfOverlappingChars;
+		}
+
+		public String getConcanatedString() {
+			return cancanatedString;
+		}
+
+		public void setCancanatedString(String cancanatedString) {
+			this.cancanatedString = cancanatedString;
+		}
+
+		public int getNumberOfOverlappingChars() {
+			return numberOfOverlappingChars;
+		}
+
+		public void setNumberOfOverlappingChars(int numberOfOverlappingChars) {
+			this.numberOfOverlappingChars = numberOfOverlappingChars;
+		}
+	}
 
 	public class VariationOf2 {
 
@@ -14,6 +45,7 @@ public class MiguelGil {
 		private int i2;
 
 		public VariationOf2(final int i1, final int i2) {
+			super();
 			this.i1 = i1;
 			this.i2 = i2;
 		}
@@ -37,38 +69,30 @@ public class MiguelGil {
 	}
 
 	public String reassemble(final String fragmentProblem) {
-		List<String> fragments = new ArrayList<String>(Arrays.asList(fragmentProblem.split(";")));
-		
+		List<String> fragments = new ArrayList<String>(Arrays.asList(fragmentProblem.split(FRAGMENT_SEPARATOR)));
+
 		while (fragments.size() > 1) {
-			List<VariationOf2> variationsOf2 = getAllVariationsOf2(fragments
-					.size());
-			//List<Integer> lengthOfCommonCharacters = new ArrayList<Integer>();
+			List<VariationOf2> variationsOf2 = getAllVariationsOf2(fragments.size());
 			int max = 0;
 			int maxPosition = 0;
 			int currentPosition = 0;
 			for (VariationOf2 variation : variationsOf2) {
-				Integer lengthOfVariationOf2 = this.countOverlapingChars(
-						fragments.get(variation.getI1()),
+				ConcatenationResult concatenationResult = this.overlapConcat2(fragments.get(variation.getI1()),
 						fragments.get(variation.getI2()));
-				//lengthOfCommonCharacters.add(lengthOfVariationOf2);
-				if (lengthOfVariationOf2 > max) {
-					max = lengthOfVariationOf2;
+				if (concatenationResult.getNumberOfOverlappingChars() > max) {
+					max = concatenationResult.getNumberOfOverlappingChars();
 					maxPosition = currentPosition;
 				}
 				currentPosition++;
 			}
-			String cancatenatedString = this.overlapConcat(
-					fragments.get(variationsOf2.get(maxPosition).getI1()),
-					fragments.get(variationsOf2.get(maxPosition).getI2()));
+			String cancatenatedString = this.overlapConcat2(fragments.get(variationsOf2.get(maxPosition).getI1()),
+					fragments.get(variationsOf2.get(maxPosition).getI2())).getConcanatedString();
 
-			if (variationsOf2.get(maxPosition).getI1() > variationsOf2.get(
-					maxPosition).getI2()) {
-				fragments.remove(variationsOf2.get(maxPosition).getI1());
-				fragments.remove(variationsOf2.get(maxPosition).getI2());
-			} else {
-				fragments.remove(variationsOf2.get(maxPosition).getI2());
-				fragments.remove(variationsOf2.get(maxPosition).getI1());
-			}
+			List<Integer> indicesToRemove = Arrays.asList(variationsOf2.get(maxPosition).getI1(),
+					variationsOf2.get(maxPosition).getI2());
+			Collections.sort(indicesToRemove, Collections.reverseOrder());
+			for (Integer index : indicesToRemove)
+				fragments.remove(index.intValue());
 
 			fragments.add(cancatenatedString);
 		}
@@ -98,62 +122,21 @@ public class MiguelGil {
 		return variationsOf2;
 	}
 
-	public String overlapConcat(String s1, String s2) {
-		// Handle nulls... never return a null
-		if (s1 == null || s1.isEmpty()) {
-			if (s2 == null || s2.isEmpty())
-				return "";
-			else
-				return s2;
-		}
-		if (s2 == null || s2.isEmpty())
-			return s1;
-		if (s1.contains(s2)) {
-			return s1;
-		}
-
-		// Checks above guarantee both strings have at least one character
-		int len1 = s1.length() - 1;
-		char last1 = s1.charAt(len1);
-		char first2 = s2.charAt(0);
-
-		// Find the first potential match, bounded by the length of s1
-		int indexOfLast2 = s2.lastIndexOf(last1,
-				Math.min(len1, s2.length() - 1));
-		while (indexOfLast2 != -1) {
-			if (s1.charAt(len1 - indexOfLast2) == first2) {
-				// After the quick check, do a full check
-				int ix = indexOfLast2;
-				while ((ix != -1)
-						&& (s1.charAt(len1 - indexOfLast2 + ix) == s2
-								.charAt(ix)))
-					ix--;
-				if (ix == -1)
-					return s1 + s2.substring(indexOfLast2 + 1);
-			}
-
-			// Search for the next possible match
-			indexOfLast2 = s2.lastIndexOf(last1, indexOfLast2 - 1);
-		}
-
-		// No match found, so concatenate the full strings
-		return s1 + s2;
-	}
-
-	public int countOverlapingChars(String s1, String s2) {
+	public ConcatenationResult overlapConcat2(String s1, String s2) {
 		// Handle nulls... never return a null
 		if (s1 == null || s1.isEmpty()) {
 			if (s2 == null || s2.isEmpty()) {
-				// return "";
-				return 0;
+				return new ConcatenationResult(EMPTY_STRING, 0);
 			} else {
-				return 0;
+				return new ConcatenationResult(s2, 0);
 			}
 		}
-		if (s2 == null || s2.isEmpty())
-			return 0;
-		if (s1.contains(s2))
-			return s2.length();
+		if (s2 == null || s2.isEmpty()) {
+			return new ConcatenationResult(s1, 0);
+		}
+		if (s1.contains(s2)) {
+			return new ConcatenationResult(s1, s2.length());
+		}
 
 		// Checks above guarantee both strings have at least one character
 		int len1 = s1.length() - 1;
@@ -161,19 +144,15 @@ public class MiguelGil {
 		char first2 = s2.charAt(0);
 
 		// Find the first potential match, bounded by the length of s1
-		int indexOfLast2 = s2.lastIndexOf(last1,
-				Math.min(len1, s2.length() - 1));
+		int indexOfLast2 = s2.lastIndexOf(last1, Math.min(len1, s2.length() - 1));
 		while (indexOfLast2 != -1) {
 			if (s1.charAt(len1 - indexOfLast2) == first2) {
 				// After the quick check, do a full check
 				int ix = indexOfLast2;
-				while ((ix != -1)
-						&& (s1.charAt(len1 - indexOfLast2 + ix) == s2
-								.charAt(ix)))
+				while ((ix != -1) && (s1.charAt(len1 - indexOfLast2 + ix) == s2.charAt(ix)))
 					ix--;
 				if (ix == -1) {
-					return indexOfLast2 + 1;
-					// return s1 + s2.substring(indexOfLast2 + 1);
+					return new ConcatenationResult(s1 + s2.substring(indexOfLast2 + 1), indexOfLast2 + 1);
 				}
 			}
 
@@ -182,8 +161,7 @@ public class MiguelGil {
 		}
 
 		// No match found, so concatenate the full strings
-		// return s1 + s2;
-		return 0;
+		return new ConcatenationResult(s1 + s2, 0);
 	}
 
 	public static void main(String[] args) {
@@ -192,7 +170,7 @@ public class MiguelGil {
 			String fragmentProblem;
 			while ((fragmentProblem = in.readLine()) != null) {
 				System.out.println(instance.reassemble(fragmentProblem));
-				System.out.println("");
+				System.out.println(EMPTY_STRING);
 
 			}
 		} catch (Exception e) {
--- src/fragment/submissions/MiguelGilTest.java
+++ src/fragment/submissions/MiguelGilTest.java
@@ -1,9 +1,10 @@
 package fragment.submissions;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.hamcrest.CoreMatchers.equalTo;
 
 public class MiguelGilTest {
 
@@ -29,12 +30,10 @@ public class MiguelGilTest {
 			+ "voluptatem.;m eius modi tem;Neque porro qui;, sed quia non numquam ei;lorem "
 			+ "ipsum quia dolor sit amet;ctetur, adipisci velit, sed quia non numq;unt ut"
 			+ "labore et dolore magnam aliquam qu;dipisci velit, sed quia non numqua;us "
-			+ "modi tempora incid;Neque porro quisquam est, qui dolorem i;uam eius modi"
-			+ "tem;pora inc;am al";
-	
+			+ "modi tempora incid;Neque porro quisquam est, qui dolorem i;uam eius modi" + "tem;pora inc;am al";
+
 	public static final String S_INPUT_TEST_CASE_3 = "m quaerat voluptatem.;pora incidunt ut labore et d;, consectetur, adipisci velit;olore magnam aliqua;idunt ut labore et dolore magn;uptatem.;i dolorem ipsum qu;iquam quaerat vol;psum quia dolor sit amet, consectetur, a;ia dolor sit amet, conse;squam est, qui do;Neque porro quisquam est, qu;aerat voluptatem.;m eius modi tem;Neque porro qui;, sed quia non numquam ei;lorem ipsum quia dolor sit amet;ctetur, adipisci velit, sed quia non numq;unt ut labore et dolore magnam aliquam qu;dipisci velit, sed quia non numqua;us modi tempora incid;Neque porro quisquam est, qui dolorem i;uam eius modi tem;pora inc;am al";
-			
-	
+
 	public static final String S_OUTPUT_TEST_CASE_2 = "Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, "
 			+ "consectetur, adipisci velit, sed quia non numquam eius modi tempora "
 			+ "incidunt ut labore et dolore magnam aliquam quaerat voluptatem.";
@@ -48,131 +47,111 @@ public class MiguelGilTest {
 
 	@Test
 	public void countOverlapingCharsWithEmptyShouldReturn0() {
-		Assert.assertThat(miguelGil.countOverlapingChars(S, S_EMPTY),
-				equalTo(0));
+		Assert.assertThat(miguelGil.overlapConcat2(S, S_EMPTY).getNumberOfOverlappingChars(), equalTo(0));
 	}
 
 	@Test
 	public void countOverlapingCharsWithMiddleFragmentShouldWork() {
-		Assert.assertThat(miguelGil.countOverlapingChars(S, S_MIDDLE),
+		Assert.assertThat(miguelGil.overlapConcat2(S, S_MIDDLE).getNumberOfOverlappingChars(),
 				equalTo(S_MIDDLE.length()));
 	}
 
 	@Test
 	public void countOverlapingCharsWithRightFragmentShouldWork() {
-		Assert.assertThat(miguelGil.countOverlapingChars(S, S_RIGHT),
-				equalTo(S_RIGHT.length()));
+		Assert.assertThat(miguelGil.overlapConcat2(S, S_RIGHT).getNumberOfOverlappingChars(), equalTo(S_RIGHT.length()));
 	}
 
 	@Test
 	public void countOverlapingCharsWithExtraRightFragmentShouldWork() {
-		Assert.assertThat(miguelGil.countOverlapingChars(S, S_EXTRA_RIGHT),
-				equalTo(3));
+		Assert.assertThat(miguelGil.overlapConcat2(S, S_EXTRA_RIGHT).getNumberOfOverlappingChars(), equalTo(3));
 	}
 
 	@Test
 	public void countOverlapingCharsWithNoMatchFragmentShouldWork() {
-		Assert.assertThat(miguelGil.countOverlapingChars(S, S_NO_MATCH),
-				equalTo(0));
+		Assert.assertThat(miguelGil.overlapConcat2(S, S_NO_MATCH).getNumberOfOverlappingChars(), equalTo(0));
 	}
-	
+
 	@Test
 	public void overlapConcatWithEmptyFragmentShouldWork() {
-		Assert.assertThat(miguelGil.overlapConcat(S, S_EMPTY), equalTo(S));
+		Assert.assertThat(miguelGil.overlapConcat2(S, S_EMPTY).getConcanatedString(), equalTo(S));
 	}
 
 	@Test
 	public void overlapConcatWithMiddleFragmentShouldWork() {
-		Assert.assertThat(miguelGil.overlapConcat(S, S_MIDDLE),
-				equalTo(S_PLUS_S_MIDDLE));
+		Assert.assertThat(miguelGil.overlapConcat2(S, S_MIDDLE).getConcanatedString(), equalTo(S_PLUS_S_MIDDLE));
 	}
 
 	@Test
 	public void overlapConcatWithRightFragmentShouldWork() {
-		Assert.assertThat(miguelGil.overlapConcat(S, S_RIGHT), equalTo(S));
+		Assert.assertThat(miguelGil.overlapConcat2(S, S_RIGHT).getConcanatedString(), equalTo(S));
 	}
 
 	@Test
 	public void overlapConcatWithExtraRightFragmentShouldWork() {
-		Assert.assertThat(miguelGil.overlapConcat(S, S_EXTRA_RIGHT),
+		Assert.assertThat(miguelGil.overlapConcat2(S, S_EXTRA_RIGHT).getConcanatedString(),
 				equalTo(S_PLUS_S_EXTRA_RIGHT));
 	}
 
 	@Test
 	public void overlapConcatWithNoMatchFragmentShouldWork() {
-		Assert.assertThat(miguelGil.overlapConcat(S, S_NO_MATCH),
-				equalTo(S_PLUS_S_NO_MATCH));
+		Assert.assertThat(miguelGil.overlapConcat2(S, S_NO_MATCH).getConcanatedString(), equalTo(S_PLUS_S_NO_MATCH));
 	}
 
 	@Test
 	public void reassembleWithOneFragmentShouldWork() {
-		Assert.assertThat(miguelGil.reassemble("london is beautiful;"),
-				equalTo("london is beautiful"));
+		Assert.assertThat(miguelGil.reassemble("london is beautiful;"), equalTo("london is beautiful"));
 	}
 
 	@Test
 	public void reassembleWithTwoNoOverlappingFragmentsShouldWork() {
-		Assert.assertThat(miguelGil.reassemble("paris ;was beautiful"),
-				equalTo("paris was beautiful"));
+		Assert.assertThat(miguelGil.reassemble("paris ;was beautiful"), equalTo("paris was beautiful"));
 	}
 
 	@Test
 	public void reassembleWithTwoOverlappingWithExtendedStringInTheRightShouldWork() {
-		Assert.assertThat(miguelGil.reassemble("london is;is beautiful"),
-				equalTo("london is beautiful"));
+		Assert.assertThat(miguelGil.reassemble("london is;is beautiful"), equalTo("london is beautiful"));
 	}
 
 	@Test
 	public void reassembleWithTwoOverlappingInTheRightFragmentsShouldWork() {
-		Assert.assertThat(miguelGil.reassemble("london is;is"),
-				equalTo("london is"));
+		Assert.assertThat(miguelGil.reassemble("london is;is"), equalTo("london is"));
 	}
 
 	@Test
 	public void reassembleWithTwoOverlappingInTheLeftFragmentsShouldWork() {
-		Assert.assertThat(miguelGil.reassemble("london is;lon"),
-				equalTo("london is"));
+		Assert.assertThat(miguelGil.reassemble("london is;lon"), equalTo("london is"));
 	}
 
 	@Test
 	public void reassembleWithTwoOverlappingWithExtendedStringOnTheLeftShouldWork() {
-		Assert.assertThat(miguelGil.reassemble("london is;paris and lon"),
-				equalTo("paris and london is"));
+		Assert.assertThat(miguelGil.reassemble("london is;paris and lon"), equalTo("paris and london is"));
 	}
 
 	@Test
 	public void reassembleWithTwoOverlappingInTheMiddleShouldWork() {
-		Assert.assertThat(miguelGil.reassemble("london and paris; and p"),
-				equalTo("london and paris"));
+		Assert.assertThat(miguelGil.reassemble("london and paris; and p"), equalTo("london and paris"));
 	}
 
 	@Test
 	public void reassembleWithFourOverlappingInOrderShouldWork() {
-		Assert.assertThat(
-				miguelGil
-						.reassemble("london and paris; and paris;paris are;are beautiful"),
+		Assert.assertThat(miguelGil.reassemble("london and paris; and paris;paris are;are beautiful"),
 				equalTo("london and paris are beautiful"));
 	}
 
 	@Test
 	public void reassembleWithFourOverlappingNoOrderShouldWork() {
-		Assert.assertThat(
-				miguelGil
-						.reassemble("and paris;london and paris; are beautiful; paris are"),
+		Assert.assertThat(miguelGil.reassemble("and paris;london and paris; are beautiful; paris are"),
 				equalTo("london and paris are beautiful"));
 	}
 
 	@Test
 	public void reassembleTestCase1ShouldWork() {
-		Assert.assertThat(miguelGil
-				.reassemble("O draconia;conian devil! Oh la;h lame sa;saint!"),
+		Assert.assertThat(miguelGil.reassemble("O draconia;conian devil! Oh la;h lame sa;saint!"),
 				equalTo("O draconian devil! Oh lame saint!"));
 	}
 
 	@Test
 	public void reassembleTestCase2ShouldWork() {
-		Assert.assertThat(miguelGil
-				.reassemble(S_INPUT_TEST_CASE_3),
-				equalTo(S_OUTPUT_TEST_CASE_2));
+		Assert.assertThat(miguelGil.reassemble(S_INPUT_TEST_CASE_3), equalTo(S_OUTPUT_TEST_CASE_2));
 	}
 }
